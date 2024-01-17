070d2c2afbafa4bf2c928e2e33e3cde6d71813f6
reza-shahriari <rezshahriari@gmail.com>
2024-01-17 14:23:06 +0330

    no mother any more!

--- Person.py
+++ Person.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class person():
+class Person():
     """person object for Family Tree 
     -----------
     Attributes:
@@ -24,61 +24,25 @@ class person():
         The person's children. None if no child. 
     """
     kind = 'person'
-    def __init__(self,name,gender,birth_day,death_day = None,mother=None,father=None,children=''):
+    def __init__(self,name,birth_day,father,death_day = None,children=''):
         self.name = name
         self.birth_day = birth_day
         self.death_day = death_day
         self.hashed_name = self.Hash(name)
-        self.mother = mother
         self.father = father
         self.children = []
-        self.gender = gender
         for i in children.strip().split(","):
             if len(i):
-                self.children.append(person(i.split(" ")[0],i.split(" ")[1],i.split(" ")[2]))
-                if self.gender == "male":
-                    self.children[-1].father = self
-                else:
-                    self.children[-1].mother = self
-    def AddChild(self, name,gender,birth_day,death_day = None,mother=None,father=None):
+                self.children.append(Person(i.split(" ")[0],i.split(" ")[1],i.split(" ")[2]))
+                self.children[-1].father = self
+                
+    def AddChild(self, name,birth_day,death_day = None):
         """add a person to this person's children.
         input: 
-            child informations (name,gender,birth_day,death_day = None,mother = None,father = None)
+            child informations (name,birth_day,death_day = None)
         return:
             None
         """
-        if self.gender == 'male':
-            father = self
-        else:
-            mother = self
-        self.children.append(person(name,gender,birth_day,death_day,mother,father))
-        
-    def FindParent(name1, name2):
-        """Gets tow persons by name and find if one is child of other one
-        input: 
-            name1,name2
-        return:
-            True if name1 is child of name2 (or vice versa) and False otherwise
-        """
-        pass
+        self.children.append(Person(name,birth_day,self,death_day,))
+                
     
-    def FindLeastCommonSubsumer(name1, name2):
-        pass
-     
-    def FindRelation(name1, name2):
-        """Gets tow persons by name and find relationships between them
-        input: 
-            name1,name2
-        return:
-            relationship 
-        """
-        pass
-    
-    def FindFarestBorn(name1):
-        pass
-    
-    def FindFarestRelations():
-        pass
-    
-    def Hash(self):
-        return ' '
