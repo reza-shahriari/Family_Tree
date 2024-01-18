d23a7eded44e24f88649a33cb09c2a481f8986ce
reza-shahriari <rezshahriari@gmail.com>
2024-01-18 11:46:50 +0330

    name generator added to create a family tree afas!

---
+++ Name.py
@@ -0,0 +1,15 @@
+import random
+class name_generator():
+    def __init__(self):
+        with open ('names/names/dist.male.txt','r') as f:
+            l = f.readlines()
+        names = []
+        for i in l:
+            names.append(i.split(' ')[0])
+        with open ('names/names/dist.female.txt','r') as f:
+            l = f.readlines()
+        for i in l:
+            names.append(i.split(' ')[0])
+        self.names = names
+    def generate(self):
+        return random.choice(self.names)
